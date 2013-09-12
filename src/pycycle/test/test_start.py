f561a80a6136a2ac5e4575d74377e0f0b22f20d7
Justin Gray <justin.s.gray@gmail.com>
2013-09-12 11:54:34 -0400

    start test works now

--- src/pycycle/test/test_start.py
+++ src/pycycle/test/test_start.py
@@ -2,6 +2,7 @@
 import unittest
 
 from openmdao.main.api import set_as_top
+from openmdao.util.testutil import assert_rel_error
 
 from pycycle import start
 
@@ -11,18 +12,18 @@ class StartTestCase(unittest.TestCase):
         comp = set_as_top(start.FlowStart())
 
         comp.W = 3.488
-        comp.Pt = .03
+        comp.Pt = 0.0272
         comp.Tt = 630.75
         comp.Mach = 1.0
 
         comp.run()
 
-        self.assertAlmostEqual(comp.Fl_O.W, 3.488)
-        self.assertAlmostEqual(comp.Fl_O.Pt, .03)
-        self.assertAlmostEqual(comp.Fl_O.Tt, 630.75)
-        #self.assertAlmostEqual(comp.Fl_O.rhos, .000074)
-        self.assertAlmostEqual(comp.Fl_O.Mach, 1.00)
-        self.assertAlmostEqual(comp.Fl_O.area, 6060.6)
+        assert_rel_error(self,comp.Fl_O.W, 3.488,.005)
+        assert_rel_error(self,comp.Fl_O.Pt, .0272, .005)
+        assert_rel_error(self,comp.Fl_O.Tt, 630.75, .005)
+        assert_rel_error(self,comp.Fl_O.rhos, .000074, .005)
+        assert_rel_error(self,comp.Fl_O.Mach, 1.00,.005)
+        assert_rel_error(self,comp.Fl_O.area, 6060.6, .005)
 
         
 if __name__ == "__main__":
